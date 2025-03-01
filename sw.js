676c239c9fb011b4070b18e25981ab82438933f2
jjosemacias <jmaciasb18@gmail.com>
2025-03-01 01:50:50 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (event) => {
     event.waitUntil(
       caches.open("demo-deprati-v1").then((cache) => {
-        return cache.addAll(["/", "/depratidemo.html", "/style.css", "/script.js"]);
+        return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
       })
     );
   });
